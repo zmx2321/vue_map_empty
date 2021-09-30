ca223abb15b93bfcea1f311e6918ae2015c6ff65
zmx2321 <zmx2321@163.com>
2021-09-30 11:07:21 +0800

    init

---
+++ src/views/test/cesiumtest/TestCesiumMap1.vue
@@ -0,0 +1,48 @@
+<template>
+    <section class="main_cont">
+        <CesiumMap />
+    </section>
+</template>
+
+<script>
+import CesiumMap from '@/components/tools/CesiumMap'
+
+export default {
+    name: "TestCesiumMap1",
+
+    components: {
+        CesiumMap,
+    },
+
+    data () {
+        return {
+            
+        }
+    },
+
+    methods: {
+        // 设置地球
+        setCesiumMap() {
+            console.log(window.cesiumViewer)
+            window.cesiumViewer.bottomContainer.style.display = "block";
+            window.cesiumViewer.scene.geocoder = true;
+        }
+    },
+
+    created() {
+        
+    },
+
+    mounted() {
+        this.setCesiumMap()
+    }
+}
+</script>
+
+<style lang="less" scoped>
+.main_cont {
+    width: 100%;
+    height: 100%;
+    padding: 0
+}
+</style>
