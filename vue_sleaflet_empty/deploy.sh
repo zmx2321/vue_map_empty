f6080c064b07205a8b985dfa4bb14efb5f476cb1
zmx2321 <zmx2321@163.com>
2021-10-09 10:21:31 +0800

    init

--- vue_sleaflet_empty/deploy.sh
+++ vue_sleaflet_empty/deploy.sh
@@ -6,16 +6,16 @@ set -e
 # 生成静态文件
 yarn build
 
-# 进入生成的文件夹
-cd vue_sleaflet_empty
+# # 进入生成的文件夹
+# cd vue_sleaflet_empty
 
-git pull
-git init
-git add -A
-git commit -m 'deploy'
+# git pull
+# git init
+# git add -A
+# git commit -m 'deploy'
 
-# 如果发布到 https://<USERNAME>.github.io  填写你刚刚创建的仓库地址
-# git push -f https://github.com/zmx2321/blog.git master:gh-pages
-git push -f git@github.com:zmx2321/vue_sleaflet_empty.git master:gh-pages
+# # 如果发布到 https://<USERNAME>.github.io  填写你刚刚创建的仓库地址
+# # git push -f https://github.com/zmx2321/blog.git master:gh-pages
+# git push -f git@github.com:zmx2321/vue_sleaflet_empty.git master:gh-pages
 
 cd -
